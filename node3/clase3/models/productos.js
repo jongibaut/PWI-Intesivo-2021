095d12ce5ba7d275f4531db88f4570d12a827bf8
jongibaut <jongibaut@gmail.com>
2021-03-10 11:27:15 -0300

    manejador de archivos

--- node3/clase3/models/productos.js
+++ node3/clase3/models/productos.js
@@ -1,14 +1,28 @@
 const pool = require('./../utils/bd');
 const TABLA_PRODUCTOS = "productos";
+const T_PRODUCTOS_IMAGENES = "productos_imagenes";
 
 //reutilizo mi funcion get del otro model y la adapto a mi nueva tabla
 const get = async () => {
-    const query = "SELECT * FROM ??";
-    const params = [TABLA_PRODUCTOS];
+    const query = "SELECT p.*, p_i.uid FROM ?? AS p JOIN ?? AS p_i ON p.id = p_i.idProducto";
+    const params = [TABLA_PRODUCTOS, T_PRODUCTOS_IMAGENES];
     // la query me devuelve un objeto o conjunto de filas Data Row Package en un [{}]
     const rows = await pool.query(query, params);
     return rows;
 }
 
+const create = async(obj) => {
+    const query = "INSERT INTO ?? SET ?";
+    const params = [TABLA_PRODUCTOS, obj]
+    const rows = await pool.query(query, params);
+    return rows;
 
-module.exports = {get}
+}
+
+const createImg = async(obj) => {
+    pool.query("INSERT INTO ?? SET ?", [T_PRODUCTOS_IMAGENES, obj]).then((response) => response).catch((e) => console.log(e));
+
+}
+
+
+module.exports = {get, create, createImg}
--- node3/clase3/routes/productos.js
+++ node3/clase3/routes/productos.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const config = {dest: `./public/tmp` };
+const upload = multer(config);
 const model = require('./../models/productos');
+const service = require('./../services/productos');
 
 const all = async(req,res) => {
     var productos = await model.get();
-    res.render('productoss', {productos});
+    res.render('productos', {productos});
+}
+const create = async(req, res) => {
+    const idFile = await service.createProducto(req.body, req.file);
+    res.redirect('/productos');
 }
 
+
 router.get('/', all);
+router.get('/create', (req,res) => res.render("crearProducto"));
+router.post('/create',upload.single("imagen"),create)
 
 module.exports = router
